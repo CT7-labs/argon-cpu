c70d2f0df6cf347d1f363bf6316563b8caea9d21
CT7-labs <biz@cherrytree7.us>
2025-02-12 15:54:02 -0500

    documentation: bus_if
    just added comments

--- hdl/common/bus_interfaces.sv
+++ hdl/common/bus_interfaces.sv
@@ -2,21 +2,23 @@ import constants_pkg::*;
 
 // interfaces
 interface master_bus_if;
-    word_t i_data;
-    word_t o_data;
+    word_t i_data; // input to module
     logic i_valid;
+    word_t o_data; // output from module
     logic o_valid;
-    logic [3:0] write_id;
-    logic [3:0] read_id;
-    logic [3:0] write_command;
-    logic [3:0] read_command;
+
+    logic [3:0] write_id; // selects which module outputs onto i_data
+    logic [3:0] read_id; // selects which module reads from i_data
+    logic [3:0] write_command; // command for the selected write module
+    logic [3:0] read_command; // command for the selected read module
 endinterface
 
 interface bus_if;
-    word_t i_data;
-    word_t o_data;
+    word_t i_data; // input to module
     logic i_valid;
+    word_t o_data; // output from module
     logic o_valid;
-    logic [3:0] command;
+
+    logic [3:0] command; // command for module
     logic [3:0] error;
 endinterface
